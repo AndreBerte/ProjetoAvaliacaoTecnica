2cfe61a05da436dad47c60c8682e3ffbd6814a60
AndreBerte <andre.fernando.berte@gmail.com>
2026-09-19 18:19:45 -0300

    fix: split unsafe Cypress command chains in support commands

--- saucedemo-cypress-cucumber/cypress/support/commands.js
+++ saucedemo-cypress-cucumber/cypress/support/commands.js
@@ -3,8 +3,12 @@ Cypress.Commands.add("login", (userType = "standard") => {
     const user = users[userType];
 
     cy.visit("/");
-    cy.get('[data-test="username"]').clear().type(user.username);
-    cy.get('[data-test="password"]').clear().type(user.password, { log: false });
+    cy.get('[data-test="username"]').clear();
+    cy.get('[data-test="username"]').type(user.username);
+
+    cy.get('[data-test="password"]').clear();
+    cy.get('[data-test="password"]').type(user.password, { log: false });
+
     cy.get('[data-test="login-button"]').click();
     cy.url().should("include", "/inventory.html");
   });
