6ab6855d97a29ad4777ab3e9fcbeb4e3680de258
AndreBerte <andre.fernando.berte@gmail.com>
2026-09-19 18:20:12 -0300

    fix: split unsafe Cypress command chains in login page

--- saucedemo-cypress-cucumber/cypress/pages/LoginPage.js
+++ saucedemo-cypress-cucumber/cypress/pages/LoginPage.js
@@ -11,8 +11,12 @@ class LoginPage {
   }
 
   login(username, password) {
-    cy.get(this.selectors.username).clear().type(username);
-    cy.get(this.selectors.password).clear().type(password, { log: false });
+    cy.get(this.selectors.username).clear();
+    cy.get(this.selectors.username).type(username);
+
+    cy.get(this.selectors.password).clear();
+    cy.get(this.selectors.password).type(password, { log: false });
+
     cy.get(this.selectors.loginButton).click();
   }
 
