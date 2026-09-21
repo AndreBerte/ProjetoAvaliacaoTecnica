1a6336233b4b459327c2da7408a2094ba0214d64
AndreBerte <andre.fernando.berte@gmail.com>
2026-09-21 16:33:15 -0300

    erro de sintaxe api

--- brasilapi-cypress-cucumber-rest-qa/cypress.config.js
+++ brasilapi-cypress-cucumber-rest-qa/cypress.config.js
@@ -24,7 +24,7 @@ async function setupNodeEvents(on, config) {
 
 module.exports = defineConfig({
   e2e: {
-    apiBaseUrl: 'https://brasilapi.com.br'
+    apiBaseUrl: 'https://brasilapi.com.br',
     specPattern: "cypress/e2e/features/**/*.feature",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents,
